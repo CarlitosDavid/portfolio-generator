7c96dfa108f212ddf99e7a41372f943efa086bf9
Carl Contreras <contrerascarl09@gmail.com>
2021-12-31 23:19:28 -0600

    created questions

--- style.js
+++ style.js
@@ -1,17 +1,24 @@
-var profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const inquirer = require('inquirer');
 
-// Notice the lack of parentheses around the 'profileDataArr' parameter?
-const printProfileData = profileDataArr => {
-    // This...
-    for (let i = 0; i < profileDataArr.length; i++) {
-        console.log(profileDataArr[i]);
+inquirer
+  .prompt([
+    /* Pass your questions in here */ {
+        type: 'input',
+        name: 'name',
+        message: 'What is your name?'
+         
     }
+  ])
+  .then(answers => console.log(answers)); 
 
-    console.log('===============');
 
-    // Is the same as this....
-    profileDataArr.forEach(profileItem => console.log(profileItem));
-};
+// const fs = require('fs');
+// const generatePage = require('./src/page-template');
 
-printProfileData(profileDataArgs);
+// const pageHTML = generatePage(name, github);
+
+// fs.writeFile('./index.html', pageHTML, err => {
+//  if (err) throw err; 
+
+    // console.log('Portfolio complete! Check out index.html to see the output!');
+// });
